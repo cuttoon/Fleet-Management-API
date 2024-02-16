f9247c8d9635091a421aaba8e429efb863332ebc
Karen Huamán <karmahucents@gmail.com>
2024-02-15 23:30:29 -0500

    add primera prueba con get

--- src/main/java/com/fleet/FleetManagementAPI/FleetManagementApiApplication.java
+++ src/main/java/com/fleet/FleetManagementAPI/FleetManagementApiApplication.java
@@ -2,12 +2,26 @@ package com.fleet.FleetManagementAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+// Agregar la anotación RestController para consultar información, ahora se volvio un constrolador
 @SpringBootApplication
+@RestController
 public class FleetManagementApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FleetManagementApiApplication.class, args);
 	}
 
+    // crear metodo, retornando una lista con información
+    @GetMapping(path = "/")
+    public List<String> getTaxis(){
+        return List.of(
+                "Karen",
+                "Huamán"
+                );
+    }
 }
